f072eb8b662ac8b011bfc52649d161db7b16d654
prairiedoggg <ssbtmdqja920@gmail.com>
2025-01-07 02:52:08 +0900

    change:nextconfig ts to js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+/@type{import {'next'}}.NextConfig/
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   output: "export",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
