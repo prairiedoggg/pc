75ccb792b17d6d5a6de00edfee783d74f62ed702
prairiedoggg <ssbtmdqja920@gmail.com>
2025-03-23 17:34:24 +0900

    feat:test

--- utils/apiClient.js
+++ utils/apiClient.js
@@ -2,10 +2,6 @@ import axios from 'axios';
 
 const apiClient = axios.create({
   baseURL: 'https://96kx3jmth9.execute-api.ap-northeast-2.amazonaws.com',
-  headers: {
-    'Accept': 'application/json'
-  },
-  withCredentials: false // CORS 정책에 따라 credentials 사용 여부 설정
 });
 
 export default apiClient;
